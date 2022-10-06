4e97a8e650c229559186d977bed491e30053207f
Sebastian Bauer <BaschtianXD@users.noreply.github.com>
2022-10-06 12:21:46 +0000

    Added conversion to opus for new sounds

--- src/SequelizeSoundProvider.ts
+++ src/SequelizeSoundProvider.ts
@@ -7,6 +7,7 @@ import Play from "./models/Play";
 import Sound from "./models/Sound";
 import * as fs from "fs"
 import Axios from "axios";
+import { exec } from "child_process";
 
 export const sequelize = new Sequelize({
     dialect: process.env.DBDIALECT! as Dialect,
@@ -55,7 +56,7 @@ export default class SequelizeSoundProvider implements ISoundProvider {
     }
 
     getPathToSound(soundId: string): Promise<string> {
-        return new Promise(resolve => resolve(this.basePath + "/" + soundId + ".mp3"))
+        return new Promise(resolve => resolve(this.basePath + "/" + soundId + ".opus"))
     }
 
     async addSoundForGuild(guildId: string, url: string, name: string, hidden: boolean): Promise<void> {
@@ -69,7 +70,34 @@ export default class SequelizeSoundProvider implements ISoundProvider {
             return Promise.reject(ErrorTypes.duplicatedName)
         }
         try {
-            await this.download(url, this.basePath + "/" + id + ".mp3")
+            // TODO
+            // download to temporary folder and convert to opus
+            // ffmpeg -i 897995667325190145.mp3 -c:a libopus -b:a 64k -vbr on -compression_level 10 -frame_duration 60 897995667325190145.opus
+            const tempFilePath = this.basePath + "/conv/" + id + ".mp3"
+            const finalFilePath = this.basePath + "/" + id + ".opus"
+            await this.download(url, tempFilePath)
+            let prom = new Promise<void>((resolve, reject) => {
+                exec("ffmpeg -i " + tempFilePath + " -c:a libopus -b:a 64k -vbr on -compression_level 10 -frame_duration 60 " + finalFilePath, (err, _stdout, _stderr) => {
+                    if (err) {
+                        reject()
+                    } else {
+                        resolve()
+                    }
+                })
+            })
+            prom.then(() => {
+                let prom = new Promise<void>((resolve, reject) => {
+                    fs.unlink(tempFilePath, (err) => {
+                        if (err) {
+                            reject()
+                        } else {
+                            resolve()
+                        }
+                    })
+                })
+                return prom
+            })
+            await prom
         } catch (err) {
             return Promise.reject(ErrorTypes.fileTooLarge)
         }
